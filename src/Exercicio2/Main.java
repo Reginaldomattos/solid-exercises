9bf88e17e26e8de15a71a71b314e527f3537a833
[Reginaldomattos] <[reginaldo.mattos@zup.com.br]>
2025-01-09 09:44:48 -0300

    fix: Correção de pequenos erros de digitação no código

--- src/Exercicio2/Main.java
+++ src/Exercicio2/Main.java
@@ -3,13 +3,13 @@ public class Main {
         Calculadora calculadora = new Calculadora();
 
         // Registrando operações
-        calculadora.registrarOperacao("soma", new soma());
+        calculadora.registrarOperacao("soma", new Soma());
         calculadora.registrarOperacao("subtracao", new Subtracao());
         calculadora.registrarOperacao("multiplicacao", new Multiplicacao());
 
         // Usando a calculadora
         System.out.println("Soma: " + calculadora.calcular("soma", 5, 3));
-        System.out.println("Subtracao: " + calculadora.calcular("subtracao", 5, 3);
+        System.out.println("Subtração: " + calculadora.calcular("subtracao", 5, 3));
         System.out.println("Multiplicação: " + calculadora.calcular("multiplicacao", 5, 3));
 
     }
