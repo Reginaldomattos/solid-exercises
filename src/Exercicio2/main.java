f7d602682ceab494a351071c4441a9d369333cdd
[Reginaldomattos] <[reginaldo.mattos@zup.com.br]>
2025-01-09 09:36:20 -0300

    fix: Registrando operações e usando a calculadora

--- src/Exercicio2/main.java
+++ src/Exercicio2/main.java
@@ -7,5 +7,10 @@ public class Main {
         calculadora.registrarOperacao("subtracao", new Subtracao());
         calculadora.registrarOperacao("multiplicacao", new Multiplicacao());
 
+        // Usando a calculadora
+        System.out.println("Soma: " + calculadora.calcular("soma", 5, 3));
+        System.out.println("Subtracao: " + calculadora.calcular("subtracao", 5, 3);
+        System.out.println("Multiplicação: " + calculadora.calcular("multiplicacao", 5, 3));
+
     }
 }
