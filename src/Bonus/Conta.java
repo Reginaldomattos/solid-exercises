438c9fe0c1c1a7157a3d054c972669eecae61b40
[Reginaldomattos] <[reginaldo.mattos@zup.com.br]>
2025-01-09 15:46:17 -0300

    feat: Implementando a classe Conta

---
+++ src/Bonus/Conta.java
@@ -0,0 +1,25 @@
+public class Conta {
+    protected double saldo;
+    protected Taxa taxa;
+
+    public Conta(Taxa taxa) {
+        this.taxa = taxa;
+    }
+
+    public void depositar(double valor) {
+        saldo += valor;
+    }
+
+    public void sacar(double valor) {
+        double taxaCalculada = taxa.calcularTaxa(valor);
+        if (saldo >= (valor + taxaCalculada));
+            saldo -= (valor + taxaCalculada);
+    } else {
+        throw new IllegalArgumentException("Saldo insuficiente!");
+    }
+}
+
+    public double getSaldo() {
+        return saldo;
+    }
+}
