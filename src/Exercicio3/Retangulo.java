0040098de46fef8ca62db8df6aacfd70bac27c7b
[Reginaldomattos] <[reginaldo.mattos@zup.com.br]>
2025-01-09 10:16:47 -0300

    feat: Adiciona @Override para realizar o calculo de Area

--- src/Exercicio3/Retangulo.java
+++ src/Exercicio3/Retangulo.java
@@ -23,4 +23,10 @@ public class Retangulo implements Forma {
     public void setAltura(int altura) {
         this.altura = altura;
     }
+
+    @Override
+    public int getArea() {
+        return  largura * altura;
+
+    }
 }
