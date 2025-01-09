0d42471fee7c3a510ba23726af77383124cee1d6
[Reginaldomattos] <[reginaldo.mattos@zup.com.br]>
2025-01-09 11:44:07 -0300

    feat: Implentando a classe Computador

---
+++ src/Exercicio5/Computador.java
@@ -0,0 +1,7 @@
+public class Computador {
+    private DispositivoEntrada dispositivoEntrada;
+
+    public Computador(DispositivoEntrada dispositivoEntrada) {
+        this.dispositivoEntrada = dispositivoEntrada;
+    }
+}
